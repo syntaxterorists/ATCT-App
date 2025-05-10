fb7d075afff23cebba80ef4e27682ac5bf5ec10c
Syntax Terrorists <syntaxterori@gmail.com>
2025-05-10 18:21:12 +0200

    Revert "First of many, adding a few splash pages and a main page."
    This reverts commit 3beec0e72dc34dda39b5b087eded772b9d8e4c38.

--- ACTC_APP_2025/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace ACTC_APP_2025;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
