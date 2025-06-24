f9d96e4f8d6609b2bdad3a1e28e4f5e83dfb2385
adintoholj <adin.toholj@gmail.com>
2025-06-24 19:29:54 +0200

    added frontend

---
+++ ATCT-Frontend/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ATCT_Frontend
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ATCT-Frontend/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ATCT_Frontend
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
